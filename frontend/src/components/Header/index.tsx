3cf71f746f17cd91bb83446f5f767b86cc11f75b
jmmarao <jmmarao@gmail.com>
2022-09-06 22:54:31 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta logo" />
+                <h1>DSMeta</h1>
+                <p>Develop by <a href="https://github.com/jmmarao">João Mateus Marão Domingues</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
